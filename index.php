57fccff1ab215daa50983a4ffc1ec7eabdb2db5d
mattskelley <matthew.skelley@tjm.com.au>
2017-09-29 07:50:49 +1000

    Stay on one page after submitting form

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 
 
-<form action="upload_csv.php" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+<form action="" method="post" accept-charset="utf-8" enctype="multipart/form-data">
 <input type="file" name="file">
 <input type="submit" name="btn_submit" value="Upload File" />
+
+
+<?php
+    
+    if ($_FILES['file']['size'] != 0) {
+    
+        $fh = fopen($_FILES['file']['tmp_name'], 'r+');
+        $lines = array();
+        while( ($row = fgetcsv($fh, 8192)) !== FALSE ) {
+            $lines[] = $row;
+        }
+        var_dump($lines);
+        
+    } else {
+        echo "Nothing to see here";
+    }
+?>
